a4d506cfcf4e6d9016f7221f77287d9d717ab121
jieyou <qinjieyou@gmail.com>
2015-01-21 20:13:27 +0800

    考虑多次重复加载lazyload时的情况

--- lazyload.js
+++ lazyload.js
@@ -35,6 +35,7 @@
             placeholder_data_img        : 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsQAAA7EAZUrDhsAAAANSURBVBhXYzh8+PB/AAffA0nNPuCLAAAAAElFTkSuQmCC',
             // for IE6\7 that does not support data image
             placeholder_real_img        : 'http://ditu.baidu.cn/yyfm/lazyload/0.0.1/img/placeholder.png'
+            // todo : 将某些属性用global来配置，而不是每次在$(selector).lazyload({})内配置
         },
         isIOS = (/(?:iphone|ipod|ipad).*os/gi).test(navigator.appVersion),
         isIOS5 = isIOS && (/(?:iphone|ipod|ipad).*os 5/gi).test(navigator.appVersion),
@@ -141,168 +142,171 @@
         })
     }
 
-    $.fn.lazyload = function(options){
-        var $elements = this,
-            $container,
-            isScrollEvent,
-            isScrollTypeEvent,
-            scrollTimer = null,
-            hasMinimumInterval
+    if(!$.fn.hasOwnProperty('lazyload')){
 
-        if(!$.isPlainObject(options)){
-            options = {}
-        }
+        $.fn.lazyload = function(options){
+            var $elements = this,
+                $container,
+                isScrollEvent,
+                isScrollTypeEvent,
+                scrollTimer = null,
+                hasMinimumInterval
 
-        // following params can be a string
-        $.each(['threshold','failure_limit','minimum_interval'],function(i,e){
-            if(type(options[e]) == 'String'){
-                options[e] = parseInt(options[e],10)
-            }
-        })
-
-        $.each(defaultOptions,function(k,v){
-            if(defaultOptions.hasOwnProperty(k) && (!options.hasOwnProperty(k) || (type(options[k]) != type(defaultOptions[k])))){
-                options[k] = v
-            }
-        })
-
-        // Cache container as jQuery as object. 
-        $container = options._$container = (!options.container || options.container == w) ? $window : $(options.container)
-        delete options.container
-
-        isScrollEvent = options.event == 'scroll'
-
-        // isScrollTypeEvent. cantains custom scrollEvent . Such as 'scrollstart' & 'scrollstop'
-        isScrollTypeEvent = isScrollEvent || options.event == 'scrollstart' || options.event == 'scrollstop'
-
-        $elements.each(function(i,e){
-            var element = this,
-                $element = $elements.eq(i),
-                placeholderSrc = $element.attr('src'),
-                originalSrcInAttr = $element.attr('data-'+options.data_attribute), // `data-original` attribute value
-                originalSrc = options.url_rewriter_fn == emptyFn?
-                    originalSrcInAttr:
-                    options.url_rewriter_fn.call(element,$element,originalSrcInAttr),
-                originalSrcset = $element.attr('data-'+options.data_srcset_attribute),
-                isImg = $element.is('img')
-
-            if($element._lazyload_loadStarted == true || placeholderSrc == originalSrc){
-                $element._lazyload_loadStarted = true
-                $elements = getUnloadElements($elements)
-                return
+            if(!$.isPlainObject(options)){
+                options = {}
             }
 
-            $element._lazyload_loadStarted = false
-
-            // If element is an img and no src attribute given, use placeholder. 
-            if(isImg && !placeholderSrc){
-                // For browsers that do not support data image.
-                $element.one('error',function(){ // `on` -> `one` : IE6 triggered twice error event sometimes
-                    $element.attr('src',options.placeholder_real_img)
-                }).attr('src',options.placeholder_data_img)
-            }
-            
-            // When appear is triggered load original image. 
-            $element.one('_lazyload_appear',function(){
-                var effectParamsIsArray = $.isArray(options.effect_params),
-                    effectIsNotImmediacyShow
-                function loadFunc(){
-                    // In most situations, the effect is immediacy show, at this time there is no need to hide element first
-                    // Hide this element may cause css reflow, call it as less as possible
-                    if(effectIsNotImmediacyShow){
-                        // todo: opacity:0 for fadeIn effect
-                        $element.hide()
-                    }
-                    if(isImg){
-                        // attr srcset first
-                        if(originalSrcset){
-                            $element.attr('srcset', originalSrcset)
-                        }
-                        if(originalSrc){
-                            $element.attr('src', originalSrc)
-                        }
-                    }else{
-                        $element.css('background-image','url("' + originalSrc + '")')
-                    }
-                    if(effectIsNotImmediacyShow){
-                        $element[options.effect].apply($element,effectParamsIsArray?options.effect_params:[])
-                    }
-                    $elements = getUnloadElements($elements)
-                }
-                if(!$element._lazyload_loadStarted){
-                    effectIsNotImmediacyShow = (options.effect != 'show' && $.fn[options.effect] && (!options.effect_params || (effectParamsIsArray && options.effect_params.length == 0)))
-                    if(options.appear != emptyFn){
-                        options.appear.call(element, $elements.length, options)
-                    }
-                    $element._lazyload_loadStarted = true
-                    if(options.no_fake_img_loader || originalSrcset){
-                        if(options.load != emptyFn){
-                            $element.one('load',function(){
-                                options.load.call(element, $elements.length, options)
-                            })
-                        }
-                        loadFunc()
-                    }else{
-                        $('<img />').one('load', function(){ // `on` -> `one` : IE6 triggered twice load event sometimes
-                            loadFunc()
-                            if(options.load != emptyFn){
-                                options.load.call(element, $elements.length, options)
-                            }
-                        }).attr('src',originalSrc)
-                    }
+            // following params can be a string
+            $.each(['threshold','failure_limit','minimum_interval'],function(i,e){
+                if(type(options[e]) == 'String'){
+                    options[e] = parseInt(options[e],10)
                 }
             })
 
-            // When wanted event is triggered load original image 
-            // by triggering appear.                              
-            if (!isScrollTypeEvent){
-                $element.on(options.event, function(){
-                    if (!$element._lazyload_loadStarted){
-                        $element.trigger('_lazyload_appear')
+            $.each(defaultOptions,function(k,v){
+                if(defaultOptions.hasOwnProperty(k) && (!options.hasOwnProperty(k) || (type(options[k]) != type(defaultOptions[k])))){
+                    options[k] = v
+                }
+            })
+
+            // Cache container as jQuery as object. 
+            $container = options._$container = (!options.container || options.container == w) ? $window : $(options.container)
+            delete options.container
+
+            isScrollEvent = options.event == 'scroll'
+
+            // isScrollTypeEvent. cantains custom scrollEvent . Such as 'scrollstart' & 'scrollstop'
+            isScrollTypeEvent = isScrollEvent || options.event == 'scrollstart' || options.event == 'scrollstop'
+
+            $elements.each(function(i,e){
+                var element = this,
+                    $element = $elements.eq(i),
+                    placeholderSrc = $element.attr('src'),
+                    originalSrcInAttr = $element.attr('data-'+options.data_attribute), // `data-original` attribute value
+                    originalSrc = options.url_rewriter_fn == emptyFn?
+                        originalSrcInAttr:
+                        options.url_rewriter_fn.call(element,$element,originalSrcInAttr),
+                    originalSrcset = $element.attr('data-'+options.data_srcset_attribute),
+                    isImg = $element.is('img')
+
+                if($element._lazyload_loadStarted == true || placeholderSrc == originalSrc){
+                    $element._lazyload_loadStarted = true
+                    $elements = getUnloadElements($elements)
+                    return
+                }
+
+                $element._lazyload_loadStarted = false
+
+                // If element is an img and no src attribute given, use placeholder. 
+                if(isImg && !placeholderSrc){
+                    // For browsers that do not support data image.
+                    $element.one('error',function(){ // `on` -> `one` : IE6 triggered twice error event sometimes
+                        $element.attr('src',options.placeholder_real_img)
+                    }).attr('src',options.placeholder_data_img)
+                }
+                
+                // When appear is triggered load original image. 
+                $element.one('_lazyload_appear',function(){
+                    var effectParamsIsArray = $.isArray(options.effect_params),
+                        effectIsNotImmediacyShow
+                    function loadFunc(){
+                        // In most situations, the effect is immediacy show, at this time there is no need to hide element first
+                        // Hide this element may cause css reflow, call it as less as possible
+                        if(effectIsNotImmediacyShow){
+                            // todo: opacity:0 for fadeIn effect
+                            $element.hide()
+                        }
+                        if(isImg){
+                            // attr srcset first
+                            if(originalSrcset){
+                                $element.attr('srcset', originalSrcset)
+                            }
+                            if(originalSrc){
+                                $element.attr('src', originalSrc)
+                            }
+                        }else{
+                            $element.css('background-image','url("' + originalSrc + '")')
+                        }
+                        if(effectIsNotImmediacyShow){
+                            $element[options.effect].apply($element,effectParamsIsArray?options.effect_params:[])
+                        }
+                        $elements = getUnloadElements($elements)
+                    }
+                    if(!$element._lazyload_loadStarted){
+                        effectIsNotImmediacyShow = (options.effect != 'show' && $.fn[options.effect] && (!options.effect_params || (effectParamsIsArray && options.effect_params.length == 0)))
+                        if(options.appear != emptyFn){
+                            options.appear.call(element, $elements.length, options)
+                        }
+                        $element._lazyload_loadStarted = true
+                        if(options.no_fake_img_loader || originalSrcset){
+                            if(options.load != emptyFn){
+                                $element.one('load',function(){
+                                    options.load.call(element, $elements.length, options)
+                                })
+                            }
+                            loadFunc()
+                        }else{
+                            $('<img />').one('load', function(){ // `on` -> `one` : IE6 triggered twice load event sometimes
+                                loadFunc()
+                                if(options.load != emptyFn){
+                                    options.load.call(element, $elements.length, options)
+                                }
+                            }).attr('src',originalSrc)
+                        }
+                    }
+                })
+
+                // When wanted event is triggered load original image 
+                // by triggering appear.                              
+                if (!isScrollTypeEvent){
+                    $element.on(options.event, function(){
+                        if (!$element._lazyload_loadStarted){
+                            $element.trigger('_lazyload_appear')
+                        }
+                    })
+                }
+            })
+
+            // Fire one scroll event per scroll. Not one scroll event per image. 
+            if(isScrollTypeEvent){
+                hasMinimumInterval = options.minimum_interval != 0
+                $container.on(options.event, function(){
+                    // desktop and Android device triggered many times `scroll` event in once user scrolling
+                    if(isScrollEvent && hasMinimumInterval && (!isIOS || options.use_minimum_interval_in_ios)){
+                        if(!scrollTimer){
+                            scrollTimer = setTimeout(function(){
+                                checkAppear($elements, options)
+                                scrollTimer = null
+                            },options.minimum_interval) // only check once in 300ms
+                        }
+                    }else{
+                        return checkAppear($elements, options)
                     }
                 })
             }
-        })
 
-        // Fire one scroll event per scroll. Not one scroll event per image. 
-        if(isScrollTypeEvent){
-            hasMinimumInterval = options.minimum_interval != 0
-            $container.on(options.event, function(){
-                // desktop and Android device triggered many times `scroll` event in once user scrolling
-                if(isScrollEvent && hasMinimumInterval && (!isIOS || options.use_minimum_interval_in_ios)){
-                    if(!scrollTimer){
-                        scrollTimer = setTimeout(function(){
-                            checkAppear($elements, options)
-                            scrollTimer = null
-                        },options.minimum_interval) // only check once in 300ms
+            // Check if something appears when window is resized. 
+            // Force initial check if images should appear when window onload. 
+            $window.on('resize load', function(){
+                checkAppear($elements, options)
+            })
+                  
+            // With IOS5 force loading images when navigating with back button. 
+            // Non optimal workaround. 
+            if(isIOS5){
+                $window.on('pageshow', function(e){
+                    if(e.originalEvent && e.originalEvent.persisted){
+                        $elements.trigger('_lazyload_appear')
                     }
-                }else{
-                    return checkAppear($elements, options)
-                }
-            })
-        }
+                })
+            }
 
-        // Check if something appears when window is resized. 
-        // Force initial check if images should appear when window onload. 
-        $window.on('resize load', function(){
-            checkAppear($elements, options)
-        })
-              
-        // With IOS5 force loading images when navigating with back button. 
-        // Non optimal workaround. 
-        if(isIOS5){
-            $window.on('pageshow', function(e){
-                if(e.originalEvent && e.originalEvent.persisted){
-                    $elements.trigger('_lazyload_appear')
-                }
+            // Force initial check if images should appear. 
+            $(function(){
+                checkAppear($elements, options)
             })
+            
+            return this
         }
-
-        // Force initial check if images should appear. 
-        $(function(){
-            checkAppear($elements, options)
-        })
-        
-        return this
     }
 })
